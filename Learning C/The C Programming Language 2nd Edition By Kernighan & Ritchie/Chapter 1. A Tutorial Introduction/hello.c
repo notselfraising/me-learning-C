25173b6bfa4c085a9c802c06e5af997eb102a6c2
carbohydration <breadbutwater@gmail.com>
2023-08-27 08:18:39 +0100

    removed comments

--- Learning C/The C Programming Language 2nd Edition By Kernighan & Ritchie/Chapter 1. A Tutorial Introduction/hello.c
+++ Learning C/The C Programming Language 2nd Edition By Kernighan & Ritchie/Chapter 1. A Tutorial Introduction/hello.c
@@ -1,6 +1,6 @@
-#include <stdio.h> /*this is a comment*/ //but this is also a comment
+#include <stdio.h>
 
-int main() { //declares the function main, needed for any program to perform their magic
-  printf("hello, world\n"); //it do be printin tho
-  return 0; //returns a value, necessary at the end of all functions, with the exception of main (the same result is got by ommitting it)
-}           //in the case of main, it returns exit code 0, meaning success
+int main() {
+  printf("hello, world\n");
+  return 0;
+}
